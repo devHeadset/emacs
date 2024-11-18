cf774223d62d65dcefe6596178d4b1c1675b57fb
devHeadset <headset@devheadset.org>
2024-11-18 17:13:43 -0500

    adding more things just look at the git history

--- config.el
+++ config.el
@@ -92,12 +92,13 @@
   (load-theme 'catppuccin :no-confirm)
 
 (use-package toc-org
-    :commands toc-org-enable
-    :init (add-hook 'org-mode-hook 'toc-org-enable))
+      :commands toc-org-enable
+      :init (add-hook 'org-mode-hook 'toc-org-enable))
 
-(add-hook 'org-mode-hook 'org-indent-mode)
-(use-package org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (add-hook 'org-mode-hook 'org-indent-mode)
+  (use-package org-bullets)
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(electric-indent-mode -1)
 
 (use-package which-key
   :init
